fa56dfddc3957af7d19d4a0463e368e385caf559
bondok26 <bondokkmg@gmail.com>
2021-01-18 13:02:53 +0200

    finished optimizer module

--- test.py
+++ test.py
@@ -3,37 +3,44 @@ from src.NN.layers.linear import Linear
 from src.NN.activations.activation import Identity, Sigmoid
 from src.NN.losses.MeanSquared import MeanSquaredLoss
 from src.opt.GD import GD
-
-x = np.array([
+from src.opt.GDExpDec import GDExpDec
+from src.opt.GDMomentum import GDMomentum
+from src.opt.GDNestrov import GDNestrov
+from src.opt.Adagrad import AdaGrad
+x = np.array([ #4x2
     [1, 1],
     [1, -1],
     [-1, 1],
     [-1, -1]
 ])
-y = np.array([
+y = np.array([ #4x1
     [-1],
     [1],
     [1],
     [-1]
 ])
-opt = GD(learning_rate=0.1)
+opt = GDNestrov(learning_rate=0.1, beta=3)
 msl = MeanSquaredLoss()
-l1 = Linear(in_dim=2, out_dim=2, activation=Identity())
-l2 = Linear(in_dim=2, out_dim=1, activation=Identity())
 
-for a in range(50):
-    out = l2(l1(x))
+l2 = Linear(in_dim=2, out_dim=3, activation=Identity())
+print(l2.bias.shape)
+for a in range(2):
+    out = l2(x)
     print("loss {}:".format(str(a)), msl(y, out))
     msl(y, out)
-    delta = np.multiply( msl.local_grad.T, l2.local_grad["dZ"])
+    delta = np.multiply(msl.local_grad.T, l2.local_grad["dZ"])
+    print(delta.shape)
     opt.optimize(l2, delta)
-    delta = np.dot(delta.T, l2.local_grad["dX"])
 
-    delta = np.multiply(delta.T, l1.local_grad["dZ"])
-    opt.optimize(l1, delta)
+    
+    delta = np.dot( l2.local_grad["dX"].T,delta)
+    print(delta.shape)
 
-print(l1.weights)
-print(l1.bias)
+    '''delta = np.multiply(delta, l1.local_grad["dZ"])
+    opt.optimize(l1, delta)'''
+
+'''print(l1.weights)
+print(l1.bias)'''
 print(l2.weights)
 print(l2.bias)
 # print(l2(l1(np.array([[5,5]]))))
